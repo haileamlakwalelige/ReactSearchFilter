2ac1de567d7ae4acba654f361616f3ae547b542c
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-02-15 00:20:07 +0300

    let's see what can it do

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable array-callback-return */
+import React from 'react';
+import './App.css';
+import JSONDATA from './MOCK_DATA.json';
+import {useState} from 'react';
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className="App">
+     <input type="text" placeholder="Search ...."  onChange={event=>{setSearchTerm(event.target.value)}}  />
+     {JSONDATA.filter((val) => {
+      if (searchTerm === ""){
+        return val;
+      }else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+     }
+     else{
+      return null;
+     }
+     }).map((val, key)=>{
+      return(
+        <div className="user" key={key}>
+          <p>{val.first_name}</p>
+        </div>
+      );
+     })}
+    </div>
+  );
+}
+
+export default App;
